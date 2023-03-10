cb9f9ec51f76735ce13acca9f8a78605642fa186
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2023-03-10 12:57:09 +1100

    Newest Version

--- Lab 2/Site/javascript/script.js
+++ Lab 2/Site/javascript/script.js
@@ -73,6 +73,7 @@ function drawBarChart() {
 }
 
 function drawDataScatterPot() {
+  // define the data points
   let dataset = [    [5, 20],
     [480, 90],
     [250, 50],
@@ -85,18 +86,12 @@ function drawDataScatterPot() {
     [220, 88]
   ];
 
+  // define the size of the chart and the margins
   var margin = { top: 20, right: 20, bottom: 30, left: 40 },
     width = 500 - margin.left - margin.right,
     height = 200 - margin.top - margin.bottom;
 
-  var x = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d[0]; })])
-    .range([0, width]);
-
-  var y = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-    .range([height, 0]);
-
+  // create an SVG element and set its size and position
   var svg = d3.select("#chart2").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -104,72 +99,37 @@ function drawDataScatterPot() {
     .attr("transform",
       "translate(" + margin.left + "," + margin.top + ")");
 
+  // add circles to represent the data points
   svg.selectAll("circle")
     .data(dataset)
     .enter()
     .append("circle")
-    .attr("cx", function(d, i) {
-      return x(d[0]);
-    })
-    .attr("cy", function(d) {
-      return y(d[1]);
-    })
-    .attr("r", 5)
-    .attr("fill", "slategrey");
+    .attr("cx", function(d) { return d[0]; }) // x-coordinate
+    .attr("cy", function(d) { return d[1]; }) // y-coordinate
+    .attr("r", 5) // radius
+    .attr("fill", "slategrey"); // color
 
+  // add labels to show the coordinates of each data point
   svg.selectAll("text")
     .data(dataset)
     .enter()
     .append("text")
-    .text(function(d) {
-      return d[0] + "," + d[1];
-    })
-    .attr("x", function(d) {
-      return x(d[0]);
-    })
-    .attr("y", function(d) {
-      return y(d[1])+20;
+    .text(function(d) { 
+      return d[0] + "," + d[1]; 
+    }) 
+    // label text
+    .attr("x", function(d) { 
+      return d[0]; 
+    }) 
+    .attr("y", function(d) { 
+      return d[1] - 10; 
     })
     .attr("font-family", "sans-serif")
     .attr("font-size", "11px")
     .attr("fill", "black")
-    .attr("text-anchor", "middle");
-
-  //X-Scale
-  var xAxis = d3.axisBottom()
-    .scale(x);
-    
-  //Y-Scale
-  var yAxis = d3.axisLeft()
-    .scale(y);
-
-  svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-  svg.append("g")
-    .call(yAxis);
-
-
-  // x-axis text
-  svg.append("text")
-    .attr("transform",
-      "translate(" + (width / 2) + " ," +
-      (height + margin.top + 10) + ")")
-    .style("text-anchor", "middle")
-    .text("X Axis");
-
-  // y-axis text
-  svg.append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left)
-    .attr("x", 0 - (height / 2))
-    .attr("dy", "1em")
-    .style("text-anchor", "middle")
-    .text("Y Axis");
-
-
+    .attr("text-anchor", "middle"); // center the text horizontally
 }
+
 
 //Need to be fixed !!! 
 // var wombatSighting = [];
@@ -183,22 +143,33 @@ function drawDataScatterPot() {
 
 // }
 
-//Test
+//Fixed
 var wombatSighting = [];
+let animals = [];
+var pets2019 = [];
+var pets2021 = [];
 
-function getData() {
+function getData(url) {
   return new Promise(function(resolve) {
-    var url = "test.csv";
     d3.csv(url, function(data) {
-      wombatSighting.push(parseFloat(data.wombats));
-      resolve(wombatSighting);
+      
+      if (url === "test.csv") {
+        wombatSighting.push(parseFloat(data.wombats));
+        resolve(wombatSighting);
+      } else if (url === "pet_ownership.csv") {
+        animals.push(data.animal);
+        pets2019.push(parseFloat(data.pets2019));
+        pets2021.push(parseFloat(data.pets2021));
+        resolve([animals, pets2019, pets2021]);
+      }
     });
   });
 }
 
 
+
 function drawWombatsBarChart() {
-  getData().then(function(wombatSighting) {
+  getData("test.csv").then(function(wombatSighting) {
     var w = 500;
     var h = 200; 
     //gap between bars
@@ -231,7 +202,7 @@ function drawWombatsBarChart() {
             return "rgb(1,50, " + d*50 + ")";
         })
     // Label
-        svg.selectAll("text")
+    svg.selectAll("text")
         .data(dataset)
         .enter()
         .append("text")
@@ -251,20 +222,156 @@ function drawWombatsBarChart() {
         .attr("fill", "white")
         .attr("text-anchor", "middle");
   });
+  
 }
 
 
+function drawPets2019() {
+  // Assume the `getData` function returns a Promise that resolves to an array of `animals`
+  getData("pet_ownership.csv").then(function(animals) {
 
-//End
+    var margin = {top: 20, right: 20, bottom: 40, left: 40};
+    var w = 1000 - margin.left - margin.right;
+    var h = 450 - margin.top - margin.bottom; 
+    //gap between bars
+    console.log(animals);
 
+    var dataset = [];
+    dataset = animals[1];
 
+    var barPadding = 1;
+    var svg = d3.select("#pets19")
+                .append("div")
+                .append("svg")
+                .attr("width", w + margin.left + margin.right)
+                .attr("height", h + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    // Draw a rectangle
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("x", function(d,i){
+            return i* (w / dataset.length);
+        })
+        .attr("y", function(d) {
+            return h - d * 8;
+        })
+        .attr("width", w/dataset.length - barPadding)
+        .attr("height", function(d){
+            return d * 8;
+        })
+        .style("fill",function(d){
+            return "rgb(1,50, " + d*8 + ")";
+        });
+
+    // Label
+    // Add labels from animals[0] under each bar
+    svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .attr("class", "label")
+        .text(function(d, i) {
+            return animals[0][i];
+        })
+        .attr("x", function(d, i) {
+            // get the text to centre of the bar
+            return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+        })
+        .attr("y", h + margin.bottom - 5)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "black")
+        .attr("text-anchor", "middle");
+    // Empty the arrays after the process is complete (Dupplicate has occured before)
+    animals = [];
+    pets2019 = [];
+    pets2021 = [];
+
+  });
+}
+
+function drawPets2021() {
+  // Assume the `getData` function returns a Promise that resolves to an array of `animals`
+  getData("pet_ownership.csv").then(function(animals) {
+
+    var margin = {top: 20, right: 20, bottom: 40, left: 40};
+    var w = 1000 - margin.left - margin.right;
+    var h = 450 - margin.top - margin.bottom; 
+    //gap between bars
+    console.log(animals[0]); //name
+
+    var dataset = [];
+    dataset = animals[2];
+
+    var barPadding = 1;
+    var svg = d3.select("#pets21")
+                .append("div")
+                .append("svg")
+                .attr("width", w + margin.left + margin.right)
+                .attr("height", h + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    // Draw a rectangle
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("x", function(d,i){
+            return i* (w / dataset.length);
+        })
+        .attr("y", function(d) {
+            return h - d * 8;
+        })
+        .attr("width", w/dataset.length - barPadding)
+        .attr("height", function(d){
+            return d * 8;
+        })
+        .style("fill",function(d){
+            return "rgb(1,50, " + d*8 + ")";
+        });
+
+    // Label
+    // Add labels from animals[0] under each bar
+    svg.selectAll("text")
+        .data(dataset)
+        .enter()
+        .append("text")
+        .attr("class", "label")
+        .text(function(d, i) {
+            return animals[0][i];
+        })
+        .attr("x", function(d, i) {
+            // get the text to centre of the bar
+            return i * (w / dataset.length) + (w / dataset.length - barPadding) / 2;
+        })
+        .attr("y", h + margin.bottom - 5)
+        .attr("font-family", "sans-serif")
+        .attr("font-size", "11px")
+        .attr("fill", "black")
+        .attr("text-anchor", "middle");
+
+    // Empty the arrays after the process is complete (Dupplicated has occured before)
+    animals = [];
+    pets2019 = [];
+    pets2021 = [];
+
+  });
+}
 
 function main() {
   text(); 
   drawBarChart(); 
   drawDataScatterPot();
   drawWombatsBarChart();
+  //getData("pet_ownership.csv");
+  //console.log(animals); console.log(pets2019);
+  drawPets2019();
+  drawPets2021();
 }
   
 
