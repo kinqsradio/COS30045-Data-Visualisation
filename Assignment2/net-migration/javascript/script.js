14e8a7deddbcea930e31e8ea0aab7ebfc134ea52
Tran Duc Anh Dang <anhdang.contact@gmail.com>
2023-05-13 16:15:23 +1000

    improving 13 May - Anh

--- Assignment2/net-migration/javascript/script.js
+++ Assignment2/net-migration/javascript/script.js
@@ -1,20 +1,115 @@
-function map() {
-    // Set up svg
+// Line chart dimensions
+var w = 1000;
+var h = 720;
+var padding = 50;
+var parseTime = d3.timeParse("%m-%y");
 
-    w = 500;
-    h = 900;
+function createScales(data) {
+  const junes = data
+    .filter(d => d.Date.split('-')[0] === "Jun")
+    .map(d => d.Date);
 
-    const projection =  d3.geoMercator()
+  const xScale = d3.scaleBand()
+    .domain(junes)
+    .range([padding, w - padding])
+    .padding(0.1);
 
-    const path = d3.geoPath()
-                   .projection(projection);
+  const yScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.NSW)])
+    .range([h - padding, padding]);
 
-    var svg = d3.select()
+  return { xScale, yScale };
 }
-function main(){
-    map();
+
+function createLineChart(data, scales) {
+  const { xScale, yScale } = scales;
+
+  const line = d3.line()
+    .x(d => xScale(d.Date) + xScale.bandwidth() / 2)
+    .y(d => yScale(d.NSW));
+
+  const svg = d3.select("#chart")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h);
+
+  svg.append("path")
+    .datum(data)
+    .attr("class", "line")
+    .attr("d", line);
+
+  addAxes(svg, xScale, yScale);
+//   addAnnotations(svg, xScale, yScale, data);
+//   createAreaChart(svg, data, xScale, yScale);
 }
 
-window.onload = function (){
+function addAxes(svg, xScale, yScale) {
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
+
+  svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ", 0)")
+    .call(yAxis);
+}
+
+function addAnnotations(svg, xScale, yScale, data) {
+  const annotationValue = d3.max(data, d => d.NSW);
+
+  svg.append("line")
+    .attr("x1", padding)
+    .attr("y1", yScale(annotationValue))
+    .attr("x2", w - padding)
+    .attr("y2", yScale(annotationValue))
+    .attr("class", "annotation-line")
+    .attr("stroke", "red")
+    .attr("stroke-dasharray", "2");
+
+  svg.append("text")
+    .attr("x", padding + 10)
+    .attr("y", yScale(annotationValue) - 7)
+    .text(`Max value: ${annotationValue}`)
+    .attr("fill", "red")
+    .attr("class", "annotation-line");
+}
+
+function createAreaChart(svg, data, xScale, yScale) {
+  const area = d3.area()
+    .x(d => xScale(d.Date))
+    .y0(h - padding)
+    .y1(d => yScale(d.NSW));
+
+  svg.append("path")
+    .datum(data)
+    .attr("class", "area")
+    .attr("d", area);
+}
+
+function main() {
+  d3.csv("/data/net-migration/net-migrant.csv").then(function(data) {
+    data.forEach(function(d) {
+      d.NSW = +d.NSW;
+      d.Vic = +d.Vic;
+      d.Qld = +d.Qld;
+      d.WA = +d.WA;
+      d.zeroline = +d.zeroline;
+    });
+
+    console.log(data);
+
+    var scales = createScales(data);
+    console.log(scales);
+    createLineChart(data, scales);
+  });
+}
+
+  
+window.onload = function() {
     main();
-}
+  };
+  
--- Assignment2/source-countries/javascript/script.js
+++ Assignment2/source-countries/javascript/script.js
@@ -1,176 +1,191 @@
 var width = 1000;
 var height = 720;
+var svg;
 
 function getColorRange() {
     return d3.scaleQuantize()
-      .range(['rgb(220, 237, 200)', 
-              'rgb(176, 212, 141)', 
-              'rgb(126, 188, 97)', 
-              'rgb(90, 174, 80)', 
-              'rgb(64, 157, 66)', 
-              'rgb(35, 139, 69)', 
-              'rgb(35, 132, 67)', 
-              'rgb(29, 115, 56)', 
-              'rgb(26, 102, 45)']);
-  }
-  
+        .range([
+            'rgb(220, 237, 200)',
+            'rgb(176, 212, 141)',
+            'rgb(126, 188, 97)',
+            'rgb(90, 174, 80)',
+            'rgb(64, 157, 66)',
+            'rgb(35, 139, 69)',
+            'rgb(35, 132, 67)',
+            'rgb(29, 115, 56)',
+            'rgb(26, 102, 45)'
+        ]);
+}
 
 function createSVG(width, height) {
-  return d3.select("#chart")
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    return d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
 }
 
 function getProjection(width, height) {
     return d3.geoMercator()
-      .translate([width / 2, height / 2])
-      .scale(width / (2 * Math.PI));  // scale for world map
-  }
+        .translate([width / 2, height / 2])
+        .scale(width / (2 * Math.PI)); // scale for world map
+}
 
 function getPath(projection) {
-  return d3.geoPath()
-    .projection(projection);
+    return d3.geoPath().projection(projection);
 }
 
 function createHeatMap(svg, path, color, json, projection, year) {
-    var countries = svg.selectAll("path")
+  svg.selectAll("path")
       .data(json.features)
       .enter()
       .append("path")
       .attr("d", path)
       .style("fill", function(d) {
-        var value = d.properties["value" + year];
-        if (d.properties.name === 'Australia') {  // Check if the country is Australia
-            return 'rgb(255, 255, 0	)';  // Use the specific color for Australia
-        }
-        return value ? color(value) : "#CCC";
-    })
-    
-      .style("stroke", "black")  // adds a black border
-      .style("stroke-width", "0.5") // width of the border
+          var value = d.properties["value" + year];
+          var formattedValue = value ? value : "Not available"; // Replace undefined values with "Not available"
+
+          if (d.properties.name === 'Australia') {
+              return 'rgb(255, 255, 0)';
+          }
+
+          return value ? color(value) : "#CCC";
+      })
+      .style("stroke", "black")
+      .style("stroke-width", "0.5")
       .on("mouseover", function(event, d) {
-        var tooltip = d3.select("#tooltip");
-        tooltip.style("visibility", "visible")
-          .html("<b>" + d.properties.name + "</b><br>" +
-            year + ": " + d.properties["value" + year])
-          .style("left", (event.pageX + 10) + "px")
-          .style("top", (event.pageY - 28) + "px");
+          var tooltip = d3.select("#tooltip");
+          tooltip.style("visibility", "visible")
+              .html("<b>" + d.properties.name + "</b><br>" + year + ": " + (d.properties["value" + year] || "Not available")) // Replace undefined values in the tooltip with "Not available"
+              .style("left", (event.pageX + 10) + "px")
+              .style("top", (event.pageY - 28) + "px");
       })
       .on("mouseout", function() {
-        var tooltip = d3.select("#tooltip");
-        tooltip.style("visibility", "hidden");
+          var tooltip = d3.select("#tooltip");
+          tooltip.style("visibility", "hidden");
       });
-
-    // Append country names
-    // svg.selectAll("text")
-    //   .data(json.features)
-    //   .enter()
-    //   .append("text")
-    //   .attr("transform", function(d) { 
-    //     return "translate(" + path.centroid(d) + ")"; 
-    //   })
-    //   .attr("dy", ".35em")
-    //   .text(function(d) {
-    //     return d.properties.name;
-    //   });
-}
-
-  
-  function loadDataAndCreateMap(svg, path, color, projection, year) {
-    d3.csv("/data/source-countries/source-countries.csv").then(function(countryData) {
-      countryData.forEach(function(d) {
-        d[year] = +d[year];
-      });
-  
-      color.domain([
-        d3.min(countryData, function(d) { return d[year]; }),
-        d3.max(countryData, function(d) { return d[year]; })
-      ]);
-  
-      d3.json("/data/source-countries/custom-geo.json").then(function(json) {
-        for (var i = 0; i < countryData.length; i++) {
-          var dataCountry = countryData[i]["Country of birth"];
-          var dataValue = countryData[i][year];
-  
-          for (var j = 0; j < json.features.length; j++) {
-            var jsonCountry = json.features[j].properties.name;
-            if (dataCountry == jsonCountry) {
-              json.features[j].properties["value" + year] = dataValue;
-              break;
-            }
-          }
-        }
-        createHeatMap(svg, path, color, json, projection, year);
-      });
-    });
-  }
-  
-
-  function createLegend(svg) {
-    var color = getColorRange();
-
-    var legendData = [
-        { text: "Australia", color: 'yellow' },
-        { text: "Low Migration", color: color.range()[0] },
-        { text: "Medium Migration", color: color.range()[color.range().length / 2 | 0] },
-        { text: "High Migration", color: color.range()[color.range().length - 1] },
-    ];
-    
-    var legend = svg.append("g")
-        .attr("transform", "translate(" + (width - 200) + "," + (height - 150) + ")");
-
-    legend.selectAll("rect")
-        .data(legendData)
-        .enter()
-        .append("rect")
-        .attr("x", 0)
-        .attr("y", function(d, i) { return i * 30; })
-        .attr("width", 20)
-        .attr("height", 20)
-        .style("fill", function(d) { return d.color; });
-
-    legend.selectAll("text")
-        .data(legendData)
-        .enter()
-        .append("text")
-        .attr("x", 30)
-        .attr("y", function(d, i) { return i * 30 + 15; })
-        .text(function(d) { return d.text; });
 }
 
 
-function main() {
-    var svg = createSVG(width, height);
+function update(year) {
+    svg.selectAll("path").remove(); // Remove existing paths
+
     var projection = getProjection(width, height);
     var path = getPath(projection);
     var color = getColorRange();
 
-    // loadDataAndCreateMap(svg, path, color, projection, "2011");
-    loadDataAndCreateMap(svg, path, color, projection, "2021");
-    createLegend(svg);  // Call the createLegend function here
+    d3.csv("/data/source-countries/source-countries.csv").then(function(countryData) {
+        countryData.forEach(function(d) {
+            d["2011"] = +d["2011"];
+            d["2021"] = +d["2021"];
+        });
+
+        color.domain([
+            d3.min(countryData, function(d) { return Math.min(d["2011"], d["2021"]); }),
+            d3.max(countryData, function(d) { return Math.max(d["2011"], d["2021"]); })
+        ]);
+
+        d3.json("/data/source-countries/custom-geo.json").then(function(json) {
+            for (var i = 0; i < countryData.length; i++) {
+                var dataCountry = countryData[i]["Country of birth"];
+                var dataValue2011 = countryData[i]["2011"];
+                var dataValue2021 = countryData[i]["2021"];
+
+                for (var j = 0; j < json.features.length; j++) {
+                  var jsonCountry = json.features[j].properties.name;
+                  if (dataCountry == jsonCountry) {
+                      json.features[j].properties["value2011"] = dataValue2011;
+                      json.features[j].properties["value2021"] = dataValue2021;
+                      break;
+                  }
+              }
+          }
+          createHeatMap(svg, path, color, json, projection, year);
+      });
+  });
+}
+
+function loadDataAndCreateMap(svg, path, color, projection) {
+  d3.csv("/data/source-countries/source-countries.csv").then(function(countryData) {
+      countryData.forEach(function(d) {
+          d["2011"] = +d["2011"];
+          d["2021"] = +d["2021"];
+      });
+
+      color.domain([
+          d3.min(countryData, function(d) { return Math.min(d["2011"], d["2021"]); }),
+          d3.max(countryData, function(d) { return Math.max(d["2011"], d["2021"]); })
+      ]);
+
+      d3.json("/data/source-countries/custom-geo.json").then(function(json) {
+          for (var i = 0; i < countryData.length; i++) {
+              var dataCountry = countryData[i]["Country of birth"];
+              var dataValue2011 = countryData[i]["2011"];
+              var dataValue2021 = countryData[i]["2021"];
+
+              for (var j = 0; j < json.features.length; j++) {
+                  var jsonCountry = json.features[j].properties.name;
+                  if (dataCountry == jsonCountry) {
+                      json.features[j].properties["value2011"] = dataValue2011;
+                      json.features[j].properties["value2021"] = dataValue2021;
+                      break;
+                  }
+              }
+          }
+          createHeatMap(svg, path, color, json, projection, "2011");
+      });
+  });
+}
+
+function createLegend(svg) {
+  var color = getColorRange();
+
+  var legendData = [
+      { text: "Australia", color: 'yellow' },
+      { text: "Low Migration", color: color.range()[0] },
+      { text: "Medium Migration", color: color.range()[Math.floor(color.range().length / 2)] },
+      { text: "High Migration", color: color.range()[color.range().length - 1] }
+  ];
+
+  var legend = svg.append("g")
+      .attr("transform", "translate(" + (width - 200) + "," + (height - 150) + ")");
+
+  legend.selectAll("rect")
+      .data(legendData)
+      .enter()
+      .append("rect")
+      .attr("x", 0)
+      .attr("y", function(d, i) { return i * 30; })
+      .attr("width", 20)
+      .attr("height", 20)
+      .style("fill", function(d) { return d.color; });
+
+  legend.selectAll("text")
+      .data(legendData)
+      .enter()
+      .append("text")
+      .attr("x", 30)
+      .attr("y", function(d, i) { return i * 30 + 15; })
+      .text(function(d) { return d.text; });
+}
+
+function main() {
+  svg = createSVG(width, height);
+  var projection = getProjection(width, height);
+  var path = getPath(projection);
+  var color = getColorRange();
+
+  loadDataAndCreateMap(svg, path, color, projection);
+  createLegend(svg);
+
+  var updateButtons = document.querySelectorAll("#update-button");
+  updateButtons.forEach(function(button) {
+      button.addEventListener("click", function() {
+          var selectedYear = button.value;
+          update(selectedYear);
+      });
+  });
 }
 
 window.onload = function() {
   main();
 };
-
-
-//   function createMap(year) {
-//     var svg = createSVG(width, height);
-//     var projection = getProjection(width, height);
-//     var path = getPath(projection);
-//     var color = getColorRange();
-  
-//     loadDataAndCreateMap(svg, path, color, projection, year);
-//   }
-  
-//   function main() {
-//     createMap("2011");
-//     createMap("2021");
-//   }
-  
-// window.onload = function() {
-//   main();
-// };
-
