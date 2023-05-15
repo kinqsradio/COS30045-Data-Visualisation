0e7f781e15c6eecea91cf6e08a6e60636d87a21e
luannn010 <103078945+luannn010@users.noreply.github.com>
2023-05-15 15:49:39 +1000

    p

--- Assignment2/net-migration/javascript/script.js
+++ Assignment2/net-migration/javascript/script.js
@@ -4,112 +4,89 @@ var h = 720;
 var padding = 50;
 var parseTime = d3.timeParse("%m-%y");
 
-function createScales(data) {
-  const junes = data
-    .filter(d => d.Date.split('-')[0] === "Jun")
-    .map(d => d.Date);
-
-  const xScale = d3.scaleBand()
-    .domain(junes)
-    .range([padding, w - padding])
-    .padding(0.1);
-
-  const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.NSW)])
-    .range([h - padding, padding]);
-
-  return { xScale, yScale };
-}
-
-function createLineChart(data, scales) {
-  const { xScale, yScale } = scales;
-
+function createLine(svg, data, xScale, yScale, property) {
   const line = d3.line()
-    .x(d => xScale(d.Date) + xScale.bandwidth() / 2)
-    .y(d => yScale(d.NSW));
-
-  const svg = d3.select("#chart")
-    .append("svg")
-    .attr("width", w)
-    .attr("height", h);
+    .x(d => xScale(parseTime(d.Date)))
+    .y(d => yScale(d[property]));
 
   svg.append("path")
     .datum(data)
-    .attr("class", "line")
-    .attr("d", line);
-
-  addAxes(svg, xScale, yScale);
-  addAnnotations(svg, xScale, yScale, data);
-  createAreaChart(svg, data, xScale, yScale);
+    .attr("class", property)
+    .attr("d", line)
+    .style("stroke", "blue");
 }
 
-function addAxes(svg, xScale, yScale) {
-  const xAxis = d3.axisBottom(xScale);
-  const yAxis = d3.axisLeft(yScale);
-
-  svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + (h - padding) + ")")
-    .call(xAxis);
-
-  svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(" + padding + ", 0)")
-    .call(yAxis);
-}
-
-function addAnnotations(svg, xScale, yScale, data) {
-  const annotationValue = d3.max(data, d => d.NSW);
-
+function addAnnotations(svg, yScale, data, text) {
   svg.append("line")
     .attr("x1", padding)
-    .attr("y1", yScale(annotationValue))
+    .attr("y1", yScale(data))
     .attr("x2", w - padding)
-    .attr("y2", yScale(annotationValue))
+    .attr("y2", yScale(data))
     .attr("class", "annotation-line")
     .attr("stroke", "red")
     .attr("stroke-dasharray", "2");
 
   svg.append("text")
     .attr("x", padding + 10)
-    .attr("y", yScale(annotationValue) - 7)
-    .text(`Max value: ${annotationValue}`)
+    .attr("y", yScale(data) - 7)
+    .text(text)
     .attr("fill", "red")
     .attr("class", "annotation-line");
 }
 
-function createAreaChart(svg, data, xScale, yScale) {
-  const area = d3.area()
-    .x(d => xScale(d.Date))
-    .y0(h - padding)
-    .y1(d => yScale(d.NSW));
-
-  svg.append("path")
-    .datum(data)
-    .attr("class", "area")
-    .attr("d", area);
-}
-
 function main() {
   d3.csv("/data/net-migration/net-migrant.csv").then(function(data) {
+    const junes = data
+      .filter(d => d.Date.split('-')[0] === "Jun")
+      .map(d => d.Date);
+
+    // Scales
+    const xScale = d3.scaleBand()
+      .domain(junes)
+      .range([padding, w - padding])
+      .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+      .domain([-80, 120])
+      .range([h - padding, padding]);
+
+    const svg = d3.select("#chart")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(0," + (h - padding) + ")")
+      .call(xAxis);
+
+    svg.append("g")
+      .attr("class", "axis")
+      .attr("transform", "translate(" + padding + ", 0)")
+      .call(yAxis);
+
+    addAnnotations(svg, yScale, 0, "ZeroLine");
+
     data.forEach(function(d) {
-    //   d.Date = +d.Date;
+      d.Date = parseTime(d.Date);
       d.NSW = +d.NSW;
       d.Vic = +d.Vic;
       d.Qld = +d.Qld;
       d.WA = +d.WA;
       d.zeroline = +d.zeroline;
+      console.log(d.NSW);
+
+      createLine(svg, data, xScale, yScale, "NSW");
+      createLine(svg, data, xScale, yScale, "Vic");
+      createLine(svg, data, xScale, yScale, "Qld");
+      createLine(svg, data, xScale, yScale, "WA");
     });
-
-    console.log(data);
-
-    var scales = createScales(data);
-    console.log(scales);
-    createLineChart(data, scales);
   });
 }
 
 window.onload = function() {
-    main();
-  };
-  
+  main();
+};
